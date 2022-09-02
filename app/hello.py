4d47d949cd244778b7e5e995477ad637672c802f
Yevhenii Redchyts <dzhudzhan@gmail.com>
2022-09-02 15:47:09 +0300

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 if config.getboolean("features", "feature_1") == True:
 	message = "Hello, Yevhenii!"
 else:
-	message = "Hello, Wooooorld!"
+	message = "Hello, Woooooorld!"
 
 @app.route("/")
 def hello():
